b078848b9f6d17c10f2e4483ff892fa671094c4c
DAlba-sudo <d.alba.work@gmail.com>
2023-09-30 16:56:43 -0400

    added gpio header file

---
+++ include/gpio.h
@@ -0,0 +1,13 @@
+#ifndef _HAL_GPIO_H
+#define _HAL_GPIO_H
+
+// This is the content for the gpio access parts of the HAL
+//
+// Description:
+//  Most of the peripherals on the BCM2711 require some sort of
+//  GPIO configuration to go along with their native Memory
+//  Mapped Registers (i.e., Mini UART).
+//
+//  This file takes care of some small abstractions related to the GPIO.
+
+#endif
